484be97f5f80b1649f94162b6837d3f0dcb915ce
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-28 18:33:52 +0700

    objek lampu rumah memanggil setsaklarno

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -20,5 +20,8 @@ int main()
 {
     remoteLampu lampuRumah;
 
-    
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
 }
