0a1b321c8f75343a7e06cdbfc9e8c71fc2934551
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-28 17:41:55 +0700

    membuat class joko pada public seseorang kemudian menampilkan pesan dari joko melalui void pesan

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -5,9 +5,20 @@ class seseorang
 {
 public:
     virtual void pesan() = 0;
-
+    //virtual void pesan()
+    // {
+    //  cout << "Pesan dari seseorang" << endl;
+    // }
 };
 
+class joko :public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "Pesan dari Joko" << endl;
+    }
+};
 int main()
 {
     std::cout << "Hello World!\n";
