91c88ad75f91e769400f272f066849e994393c90
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-28 18:41:34 +0700

    virtual (penumpukan) void perkenalan pada akses modifier public

---
+++ noOverriding/noOverriding.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class baseClass
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
