f329257cc5f77479a9cb994d9dbbff0af954d372
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-28 18:54:06 +0700

    method pengisi nilai dan a adalah objek x dan b adalah objek y

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -4,6 +4,15 @@ using namespace std;
 class AbstaksiKlas
 {
 private: string x, y;
+
+public:
+    // method unt mengisi nilai
+    //private member
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
 };
 
 int main()
