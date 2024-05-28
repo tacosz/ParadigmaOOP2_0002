9adaf11e3ab35e74307a7720e06c0ce858ad7408
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-28 19:02:01 +0700

    class orang terdapat akses modifier public

---
+++ multiwaris/multiwaris.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class orang
+{
+public:
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << " orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
